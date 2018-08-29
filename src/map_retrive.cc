7875d790013102fb5316546cf84890279bd85f2c
Devendra Dahiphale <ddahipha@cisco.com>
2018-08-29 12:28:36 -0400

    Add a test function for using template

--- src/map_retrive.cc
+++ src/map_retrive.cc
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+template <typename T>
+void template_test(T t)
+{
+    cout << "Just printing whatever is sent : " << t << endl;
+}
+
+template void template_test<int>(int);
+template void template_test<string>(string);
+
 int get_value(map<int, int> my_map, int key) {
     if(my_map.find(key) != my_map.end())
         return my_map[key];
