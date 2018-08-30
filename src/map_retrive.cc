b91714f9ec47120ff9ed6c6514a8ded1b7a8b793
Devendra Dahiphale <ddahipha@cisco.com>
2018-08-30 00:15:08 -0400

    Remove dummy functions

--- src/map_retrive.cc
+++ src/map_retrive.cc
@@ -21,7 +21,7 @@ void print_custom(const vector<int> &val)
 }
 
 template<typename T, typename S>
-void findkey(map<T, S> &smap, T key) 
+void get_value(map<T, S> &smap, T key) 
 {
    typedef map<T, S> container;
    std::ostringstream sStream;
@@ -35,26 +35,5 @@ void findkey(map<T, S> &smap, T key)
    return;
 }
 
-template<typename T>
-T print_string(T var) {
-    string var1 = "pratik";
-    if(var == var1)
-        cout << "It is working" << endl;
-    return var;
-}
-
-void func_dummy() {
-    map<int, vector<int>> Mvec;
-    map<int, int> Mint;
-    print_string("abc");
-    findkey(Mvec, 0);
-    findkey(Mint, 0);
-}
-
-int print_key(map<int, int> M, int key)
-{
-    cout << "Maps size is:" << M.size() << endl;
-    cout << "Key:" << " " << key << " " << M[key] << endl;
-    return 0;
-}
-
+template void get_value<int,int>(map<int,int>, int);
+template void get_value<int, vector<int>(map<int, vector<int>>, int);
