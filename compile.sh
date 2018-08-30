ce719e6cbb6fe1918ef1adb9cadbdffa7ef19b6f
Devendra Dahiphale <ddahipha@cisco.com>
2018-08-30 00:23:42 -0400

    Update the compile.sh script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build/lib/
 
-g++ -Wall -fPIC -c src/map_retrive.cc -o build/map_retrive.o
-g++ -shared -Wl,-soname,libmap.so -o build/lib/libmap.so build/map_retrive.o 
+g++ -Wall -fPIC -c src/map_retrieve.cc -o build/map_retrieve.o
+g++ -shared -Wl,-soname,libmap.so -o build/lib/libmap.so build/map_retrieve.o 
